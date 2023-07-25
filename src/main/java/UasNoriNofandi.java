f01e3fa078da86a4cdc01d85f56d3ab54aae7f9e
nori07 <52597119+use63@users.noreply.github.com>
2023-07-26 00:44:51 +0700

    Add files via upload

---
+++ src/main/java/UasNoriNofandi.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Nori07
+ */
+public class UasNoriNofandi {
+
+    public static void main(String[] args) {
+        AplikasiKasirGUI.main(args);
+    }
+}
